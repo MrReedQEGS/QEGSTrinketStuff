77053f91bb00cc7b106ff448e44a8b184999ff67
MrReedQEGS <60434301+MrReedQEGS@users.noreply.github.com>
2026-01-21 10:28:59 +0000

    Create step 2b - using the index to print a list.py

---
+++ KS3/lessons/lists/step 2b - using the index to print a list.py
@@ -0,0 +1,15 @@
+###############################################################
+# HOW TO PRINT A WHOLE LIST USING THE POSITION OF EACH ITEM
+#
+# Written by Mr Reed
+###############################################################
+
+myListOfThings = ["cat","dog","horse","spider","ant","bear"]
+
+#print a title
+print("My list of things:")
+
+for i in range(0,6):
+  print(myListOfThings[i])
+  
+#THIS IS USEFUL LATER ON IF YOU WANT TO NUMBER EACH THING...
