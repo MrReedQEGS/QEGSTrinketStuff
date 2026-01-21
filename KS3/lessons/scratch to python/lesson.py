07cb7e84661ab70bc1b43da83c824654f680ab72
MrReedQEGS <60434301+MrReedQEGS@users.noreply.github.com>
2026-01-21 10:23:51 +0000

    Create lesson.py

---
+++ KS3/lessons/scratch to python/lesson.py
@@ -0,0 +1,15 @@
+someTitle = "I will multiply two numbers for you..."
+someMessage = ""
+num1 = 0
+num2 = 0
+theAnswer = 0
+someText = ""
+
+print(someTitle)
+num1 = int(input("Give me a number: "))
+num2 = int(input("Give me another number: "))
+theAnswer = num1 * num2
+
+someText = str(num1) + " X " + str(num2) + " = " + str(theAnswer)
+print(someText)
+print(":)")
