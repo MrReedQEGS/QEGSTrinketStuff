2adf1bd1a93194a2e6e0e66459c93e1f318030c8
MrReedQEGS <60434301+MrReedQEGS@users.noreply.github.com>
2026-01-21 11:59:06 +0000

    Create DrawSquareTheLongWay.py

---
+++ GCSE/Turtle/DrawSquareTheLongWay.py
@@ -0,0 +1,18 @@
+#make a turtle
+import turtle
+bob = turtle.Turtle()
+
+#Do your drawing the long way by repeating the code manually 4 times.
+
+bob.forward(100)
+bob.left(90)
+
+bob.forward(100)
+bob.left(90)
+
+bob.forward(100)
+bob.left(90)
+
+bob.forward(100)
+bob.left(90)
+
